57ad8cb4901a92993223d4a4db8e4c27143731bb
Dulaj <shad3s.of.m3@gmail.com>
2022-05-16 16:10:34 +0530

    complete map origin part

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -4,6 +4,8 @@ import { FlatList, Image, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
   {
@@ -21,6 +23,7 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const origin = useSelector(selectOrigin);
   const navigation = useNavigation();
   return (
     <FlatList
@@ -31,9 +34,10 @@ const NavOptions = () => {
       renderItem={({ item }) => (
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
+          disabled={!origin}
           style={tw`p-2 pl-6 pb-8 pt-4 w-40 m-2 bg-gray-200`}
         >
-          <View>
+          <View style={tw`${!origin && item.id == "123" && "opacity-20"}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{
