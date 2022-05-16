bcaae52b8b305175ec13b1441c745d00b17ac64d
Dulaj <shad3s.of.m3@gmail.com>
2022-05-16 16:50:11 +0530

    draw line bw origin and destination

---
+++ components/NavigateCard.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, Dulaj</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <GooglePlacesAutocomplete
+          styles={toInputBoxStyles}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          minLength={2}
+          onPress={(data, details = null) => {
+            dispatch(
+              setDestination({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            navigation.navigate("RideOptionsCard");
+          }}
+          fetchDetails={true}
+          returnKeyType="search"
+          enablePoweredByContainer={false}
+          placeholder="Where to?"
+          debounce={400}
+          nearbyPlacesAPI="GooglePlacesSearch"
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#dddddf",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,6 +3,8 @@ import React from "react";
 import tw from "twrnc";
 import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
@@ -16,6 +18,13 @@ const MapScreen = () => {
           <Stack.Screen
             name="NavigateCard"
             component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
             options={{
               headerShown: false,
             }}
