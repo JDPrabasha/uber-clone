74030d6dccbb3cea7f5559ba0219ccfbf2a131a9
Dulaj <shad3s.of.m3@gmail.com>
2022-05-16 11:12:22 +0530

    add navigation

---
+++ components/NavOptions.js
@@ -0,0 +1,56 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { FlatList, Image, TouchableOpacity } from "react-native-web";
+import tw from "twrnc";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 w-40 m-2 bg-gray-200`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{
+                uri: item.image,
+              }}
+            />
+            <Text style={tw`text-lg font-semibold mt-2`}>{item.title}</Text>
+            <AntDesign
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              size={24}
+              color="white"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
